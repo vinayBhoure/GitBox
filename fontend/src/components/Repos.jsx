e6e2fbe400c5295513c81dadeb071fb63965d2f9
Vinay Bhoure <bhoure21@gmail.com>
2024-03-07 01:42:44 +0530

    managing width for repo component

--- fontend/src/components/Repos.jsx
+++ fontend/src/components/Repos.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Repo from './Repo'
 
-function Repos({userRepos}) {
+function Repos({userRepos, fullScreen = false}) {
+
+  const screenSize = fullScreen ? "w-full" : 'lg:w-2/3 w-full'
   return (
-    <div className=' lg:w-2/3 w-full bg-glass rounded-lg px-8 py-6'>
+    <div className= {`${screenSize} bg-glass rounded-lg px-8 py-6`}>
       <ol className='relative border-s border-gray-200' >
        {userRepos.map((repo)=>{
         return <Repo key={repo.id} repo={repo}/>
