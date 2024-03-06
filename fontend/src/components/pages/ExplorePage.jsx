37e7291a8ac08221c49f334683c7ba9e28d56bd9
Vinay Bhoure <bhoure21@gmail.com>
2024-03-07 01:42:03 +0530

    fetching data for explore page

--- fontend/src/components/pages/ExplorePage.jsx
+++ fontend/src/components/pages/ExplorePage.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import Spinner from "../Spinner";
+import Repos from "../Repos";
+import { useState } from "react";
 
 function ExplorePage() {
+  const [repo, setRepo] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [language, setLanguage] = useState("javascript");
+  const searchHandler = async (e, language) => {
+    setLoading(true);
+    setRepo([]);
+    try {
+      const res = await fetch(
+        `https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc&per_page=10`,
+        {
+          headers: {
+            authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
+          },
+        }
+      );
+      const repo = await res.json();
+      setRepo(repo.items);
+    } catch (err) {
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="px-4">
       <div className="bg-glass max-w-2xl mx-auto rounded-md p-4">
@@ -12,28 +38,35 @@ function ExplorePage() {
             src="/javascript.svg"
             alt="JavaScript"
             className="h-11 sm:h-20 cursor-pointer"
+            onClick={(e) => searchHandler(e, "javascript")}
           />
           <img
             src="/typescript.svg"
             alt="TypeScript logo"
             className="h-11 sm:h-20 cursor-pointer"
+            onClick={(e) => searchHandler(e, "typescript")}
           />
           <img
             src="/c++.svg"
             alt="C++ logo"
             className="h-11 sm:h-20 cursor-pointer"
+            onClick={(e) => searchHandler(e, "c++")}
           />
           <img
             src="/python.svg"
             alt="Python logo"
             className="h-11 sm:h-20 cursor-pointer"
+            onClick={(e) => searchHandler(e, "python")}
           />
           <img
             src="/java.svg"
             alt="Java logo"
             className="h-11 sm:h-20 cursor-pointer"
+            onClick={(e) => searchHandler(e, "java")}
           />
         </div>
+        {!loading && repo.length > 0 && <Repos userRepos={repo} fullScreen />}
+        {loading && <Spinner />}
       </div>
     </div>
   );
