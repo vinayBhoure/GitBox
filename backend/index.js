5ddf46b3ed89db686c54e79d0cb36411e96a1493
Vinay Bhoure <bhoure21@gmail.com>
2024-03-08 20:03:51 +0530

    changing localhost port

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@
 
  // Load environment variables
 require('dotenv').config();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 // using statc site
 // app.use(express.static('dist'));
